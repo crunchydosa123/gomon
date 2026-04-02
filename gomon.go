13aea814b7f7969d90b8dc974e6073c3d39b0e82
Pratham Gadkari <prathamgadkari@gmail.com>
2026-04-02 17:39:33 +0530

    added http polling

--- gomon.go
+++ gomon.go
@@ -1,6 +1,7 @@
 package gomon
 
 import (
+	"net/http"
 	"time"
 )
 
@@ -30,6 +31,12 @@ func New(opts ...Option) *Gomon {
 	}
 }
 
+func (g *Gomon) Start() {
+	for _, s := range g.services {
+		go g.pollService(s)
+	}
+}
+
 func (g *Gomon) Register(s Service) {
 	g.services = append(g.services, s)
 }
@@ -41,4 +48,51 @@ func (g *Gomon) Stop() {
 
 func (g *Gomon) Subscribe() <-chan Metric {
 	return g.metricsCh
+}
+
+func (g *Gomon) pollService(s Service) {
+	ticker := time.NewTicker(s.Interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			metric := g.checkHTTP(s)
+
+			select {
+			case g.metricsCh <- metric:
+			case <-g.stopCh:
+				return
+			}
+
+		case <-g.stopCh:
+			return
+		}
+	}
+}
+
+func (g *Gomon) checkHTTP(s Service) Metric {
+	start := time.Now()
+
+	resp, err := http.Get(s.URL)
+	latency := time.Since(start)
+
+	if err != nil {
+		return Metric{
+			ServiceName: s.Name,
+			Status:      0,
+			Latency:     latency,
+			Error:       err,
+			Timestamp:   time.Now(),
+		}
+	}
+
+	defer resp.Body.Close()
+
+	return Metric{
+		ServiceName: s.Name,
+		Status:      resp.StatusCode,
+		Latency:     latency,
+		Timestamp:   time.Now(),
+	}
 }
